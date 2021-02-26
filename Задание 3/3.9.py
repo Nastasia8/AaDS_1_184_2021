d45e0b1dc09fc5846f5a05a1ead5c485e1c17d20
DMITRII <presnovdmitriy02@gmail.com>
2021-02-26 10:58:47 +0300

    add2

---
+++ Задание 3/3.9.py
@@ -0,0 +1,25 @@
+#9
+A = [15, 86, 9, 21, -97, -88, 0, -27, 63, -17, -44, -5]
+d = {}
+s=''
+print(" С помощью цикла")
+for w in A:
+    if w <0:
+        d[w]=['negative']
+        s = w
+    elif w==0:
+        d[w] = ['zero']
+        s = w
+    elif w>0:
+        d[w] = ['positive']
+        s = w
+d[s].append(w)
+print(d)
+print("C помощью Dictionary comperhention")
+print({A:"+" if A>0 else "-" if A<0 else "zero" for A in A})
+
+
+
+
+
+
