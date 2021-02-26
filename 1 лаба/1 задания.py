4038c4e8aa3fe0ce94fd6b2d1524223e71f3e2eb
Valera052002 <71351052+Valera052002@users.noreply.github.com>
2021-02-26 19:03:03 +0300

    Dmitriev Valera

---
+++ 1 лаба/1 задания.py
@@ -0,0 +1,11 @@
+text="The tiger once ranged widely from the Eastern Anatolia Region in the west to the Amur River basin, and in the south from the foothills of the Himalayas to Bali in the Sunda islands."
+x=0
+y=0
+for letter in text:
+    if letter == "s":
+        x+=1
+    if letter == "a":
+        y+=1
+print(x)
+print(y) 
+ 
