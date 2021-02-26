235ab55f69c34cbcb08f88274601b9faac745fc7
DMITRII <presnovdmitriy02@gmail.com>
2021-02-26 23:02:41 +0300

    add.

--- Задание 3/3.8.py
+++ Задание 3/3.8.py
@@ -1,23 +1,9 @@
-from random import random
-M = 10
-N = 5
-a = []
-for i in range(N):
-    b = []
-    for j in range(M):
-        b.append(round(random()*9))
-    a.append(b)
-    print(b)
-print("-------")
-e=0
-k=1
-s=1
-for i in range(N):
-    b = []
-    for j in range(M)[::-1]:
-        b.append(round(random() * 9))
-        a.append(b)
-        print(b)
+array = [[7, 5, 9], [0, 1, 2,], [4, 7, 2]]
+
+for i in array:
+    for j in i[::-1]:
+        print(array[i][j])
+    print()
 
 
 
---
+++ Яндекс/yandex.5.py
@@ -0,0 +1,7 @@
+a={}
+n=int(input())
+s=input()
+n=s.split()
+for i in n:
+    a[int(i)]=1
+print(len(a))
