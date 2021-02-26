5731f25af191615b07243912e87c42517380f7a9
Никита Баранов <39243262+nkkkbrrr@users.noreply.github.com>
2021-02-27 00:51:31 +0300

    commit

---
+++ task 1 povtorenie_1/task 04.py
@@ -0,0 +1,8 @@
+def findS(P, m, n):
+    i = 15
+    if P > 0 and (m == 3 or m == 6 or m == 12) and n > 0:
+        return P * (1 + (l/100)/(m/12))**(m/12*n)
+    else:
+        print("error")
+
+print("S =", findS(int(input("Enter P")), int(input("Enter m")), int(input("Enter n"))))
