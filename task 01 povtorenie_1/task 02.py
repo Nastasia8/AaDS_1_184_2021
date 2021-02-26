8ffead730ecffc1de53d4f5aa62542307912b8f2
Никита Баранов <39243262+nkkkbrrr@users.noreply.github.com>
2021-02-27 01:46:45 +0300

    Delete task 01 povtorenie_1 directory

--- task 01 povtorenie_1/task 02.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def calculate(l, g = 9.8):
-    if g != 0:
-        return 2 * math.pi * math.sqrt(l * g)
-    else:
-        print("error")
-
-print("Answer :", calculate(int(input("Enter l value: ")))
