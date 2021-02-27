77b2776205f31f50550ffdcbe65aca0cbb575764
DMITRII <presnovdmitriy02@gmail.com>
2021-02-27 08:30:02 +0300

    add..

--- Задание 3/3.8.py
+++ Задание 3/3.8.py
@@ -1,12 +1,18 @@
 array = [[7, 5, 9], [0, 1, 2,], [4, 7, 2]]
 
-for i in array:
-    for j in i[::-1]:
-        print(array[i][j])
-    print()
+#for i in number:
+#    for j in i[::-1]:
+#       print(number[i][j])
+#   print()
+
+def funct(number):
+    for arr in array:
+        for j in range(len(arr)//2):
+            arr[j],arr[-j-1]=arr[-j-1],arr[j]
+        return array
 
 
-
+print(funct(array))
 
 
 
