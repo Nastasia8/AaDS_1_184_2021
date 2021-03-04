0d6c82f3b01693b9d56651dda7141da748729e8c
Никита Баранов <39243262+nkkkbrrr@users.noreply.github.com>
2021-03-04 20:02:06 +0300

    Delete task 01 povtorenie_1 directory

--- task 01 povtorenie_1/task 04.py
+++
@@ -1,9 +0,0 @@
-def findS(P, m, n):
-    i = 15
-    if P > 0 and (m == 3 or m == 6 or m == 12) and n > 0:
-        return P * (1 + (i/100)/(m/12))**(m/12*n)
-    else:
-        print("error")
-        return 0
-
-print("S =", findS(int(input("Enter P")), int(input("Enter m")), int(input("Enter n"))))
