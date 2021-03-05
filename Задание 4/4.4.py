b8998e4863f3740a195e046e171966927fe29697
DMITRII <presnovdmitriy02@gmail.com>
2021-03-05 11:28:53 +0300

    :))

--- Задание 4/4.4.py
+++ Задание 4/4.4.py
@@ -1,11 +1,23 @@
+#1 способ
 def choose(n, k):
     if k in (0, n):
         return 1
     return choose(n - 1, k - 1) + choose(n - 1, k)
-for row in range(40):
+for row in range(10):
     for k in range(row+1):
         print(choose(row, k), end=' ')
     print()
 
+#2 cпособ
 
-    
+def vizov(h):
+    b=[1]
+    a=[0]
+    for j in range(h):
+        print(b)
+        b=[left+right for left, right in zip(b+a,a+b)]
+    return(b)
+
+
+h=10
+vizov(h)
