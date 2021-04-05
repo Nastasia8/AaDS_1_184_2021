c660d8038a347482e969356aa7e4c14d0f9c12e8
DianaKurbanova <71451653+DianaKurbanova@users.noreply.github.com>
2021-04-06 01:40:11 +0300

    Add files via upload

---
+++ модуль 2. задание 6.py
@@ -0,0 +1,13 @@
+size1 = int(input())
+a = list(int(i) for i in input().split(" "))[:size1]
+size2 = int(input())
+b = list(int(i) for i in input().split(" "))[:size2]
+
+for i in range(0, size2):
+    a[b[i]-1] -= 1
+
+for i in range(0, size1):
+    if (a[i] < 0):
+        print("yes")
+    else:
+        print("no")
