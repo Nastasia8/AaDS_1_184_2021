bd3c3f7ae4bba4ae3eef6df7e2bd8d1f98bf5b76
Sasha_Kulikova <aleksandrakulikovass@mail.ru>
2021-04-05 18:39:51 +0300

    povtor

---
+++ Повторение2.py
@@ -0,0 +1,24 @@
+from collections import deque
+n = int(input())
+q1 = deque()
+q2 = deque()
+q3 = deque()
+q4 = deque()
+for i in range(n):
+    num, surname = map(str, input().split())
+    if int(num)==1:
+        q1.append(surname)
+    elif int(num)==2:
+        q2.append(surname)
+    elif int(num)==3:
+        q3.append(surname)
+    else:
+        q4.append(surname)
+while q1:
+    print(1, q1.popleft())
+while q2:
+    print(2, q2.popleft())
+while q3:
+    print(3, q3.popleft())
+while q4:
+    print(4, q4.popleft())
