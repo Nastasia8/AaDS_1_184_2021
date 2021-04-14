f67aa0690b626a02aae23e46c2cf132a707736a6
Tolkachov12 <71261711+Tolkachov12@users.noreply.github.com>
2021-04-14 15:33:41 +0300

    2.7 доделал
    Добавил пояснения к каждой строчке кода, как и просили, а так же чуть-чуть упростил(на одну строчку)

--- у.с.2.(7).py
+++ у.с.2.(7).py
@@ -1,33 +1,33 @@
-Array = []
-length = int(input())
-for i in range(length):
-    Array.append(input())
-print("Initial array:")
-print(', '.join(Array))
-Count = len(Array[0])
-Phase=0
-Rang=10
-for i in range(Count-1,-1,-1):
-    Phase+=1
-    print('**********')
-    print(f'Phase {Phase}')
-    bucket = [[] for _ in range(Rang)]
-    for j in range(len(Array)):
-        bucket[ord(Array[j][i]) - ord('0')].append(Array[j])
-    for j in range(Rang):
-        if len(bucket[j])==0:
-            print(f'Bucket {j}: empty')
-        else:
-            print("Bucket "+str(j)+": ", end='')
-            for now in range(len(bucket[j])-1):
-                print(bucket[j][now],end=', ')
-            print(bucket[j][len(bucket[j])-1])
-    p = 0
-    for j in range(Rang):
-        for k in range(len(bucket[j])):
-          Array[p] = bucket[j][k]
-          p += 1
+Array = [] #Создаём пустой массив
+length = int(input()) #Ввод кол-ва чисел, которые мы будет вводить
+for i in range(length): #Цикл for, который идёт до длинный lenght
+    Array.append(input()) #Пишем числа и добавляем их в массив
+print("Initial array:") #Выводим на экран Initial array:
+print(', '.join(Array)) #Выводятся наши числа через запятую
+Count = len(Array[0]) #Длинна первого элемента
+Phase=0 #
+Rang=10 #кол-во Busket
+for i in range(Count-1,-1,-1): #Цикл for, который идёт в обратном направлении от Count-1 до -1 с шагом -1
+    Phase+=1 # Прибавляем к фазе 1
+    print('**********') #Выводим **********
+    print(f'Phase {Phase}') #Выводим Phase и число фазы
+    bucket = [[] for _ in range(Rang)] #Создаём список размером 10 элементов
+    for j in range(len(Array)): #Цикл for до конца массива
+        bucket[ord(Array[j][i]) - ord('0')].append(Array[j]) #Вычисляем индекс с помощью функции ord и записывает в него j-ый элемент
+    for j in range(Rang): #Цикл for до 10
+        if len(bucket[j])==0: #Условие при котором длина корзины равна 0
+            print(f'Bucket {j}: empty') #Выводим Bucket (номер): empty
+        else: #Иначе
+            print("Bucket "+str(j)+": ", end='') #Выводим Bucket (номер): 
+            for now in range(len(bucket[j])): #Цикл for до длины карзины номера
+                print(bucket[j][now],end=', ') #Выводим число,которое находится в карзине
+            print() #Переход на новую строку
+    p = 0 #Вводим переменную
+    for j in range(Rang): #Цикл for до 10
+        for k in range(len(bucket[j])): #Цикл до длины корзины
+          Array[p] = bucket[j][k] #к массиву переменной p присваивается число из карзины с индуксов j
+          p += 1 #Увеличиваем переменную p на едиинцу
  
-print('**********')
-print("Sorted array:")
-print(', '.join(Array))
+print('**********') #Выводим на экран **********
+print("Sorted array:") #Выводим на экран Sorted array:
+print(', '.join(Array)) #Выводим полученный результат
