e48e1c302f68da164fc1bfaf8ccef4df23aa0e0d
DianaKurbanova <71451653+DianaKurbanova@users.noreply.github.com>
2021-05-12 00:00:15 +0300

    Add files via upload

---
+++ Модуль 4.4.py
@@ -0,0 +1,18 @@
+def minimum(num, win, arr):
+    stack = []
+    for i in range(win):
+        while stack and arr[i] <= arr[stack[-1]]:
+            stack.pop()
+        stack.append(i)
+    for i in range(win, num):
+        print(arr[stack[0]])
+        while stack and i - win >= stack[0]:
+            stack.pop(0)
+        while stack and arr[i] <= arr[stack[-1]]:
+            stack.pop()
+        stack.append(i)
+    print(arr[stack[0]])
+
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+minimum(n, k, a)
