fef96e5fd16efe1b3e8f101f844bcae7c311d2eb
Никита Баранов <39243262+nkkkbrrr@users.noreply.github.com>
2021-05-24 11:19:27 +0300

    commit

--- yandex contest 4/task 02.py
+++ yandex contest 4/task 02.py
@@ -3,22 +3,6 @@
 from collections import deque
 size = int(input())
 a = deque(list(map(int, input().split(" ")))[:size])
-inOrder = True
-
-for _ in range(len(a)):
-    if a[0] == max(a) or a[0] == min(a):
-        a.popleft()
-    else:
-        inOrder = False
-        break
-
-print("YES" if inOrder else "NO")
-
-# Решение которое вы мне показали
-
-from collections import deque
-size = int(input())
-a = deque(list(map(int, input().split(" ")))[:size])
 b = []
 deadblock = []
 res = list(range(1, size+1))
