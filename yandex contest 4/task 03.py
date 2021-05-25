2dfdbbe2353389a62f732565fd0516359f1b71cd
Никита Баранов <39243262+nkkkbrrr@users.noreply.github.com>
2021-05-26 00:46:44 +0300

    commit

--- yandex contest 4/task 03.py
+++ yandex contest 4/task 03.py
@@ -1,21 +1,10 @@
-size = int(input())
-a = list(int(i) for i in input().split(" "))[:size]
-last_elements = {a[len(a) - 1] : len(a) - 1}
-output = [-1]
-
-for i in reversed(range(0, len(a) - 1)):
-    if a[i] == min(last_elements):
-        last_elements.update({a[i] : i})
-        output.append(-1)
-    else:
-        appended = False
-        for j in reversed(last_elements.keys()):
-            if j < a[i]:
-                output.append(last_elements[j])
-                appended = True
-                break
-        if not appended:
-            output.append(-1)
-        last_elements.update({a[i] : i})
-
-print(*reversed(output))
+n = int(input())
+nums = list(map(int, input().split()))[:n]
+stack = []
+indx = [0]*n
+for i in range(n-1, -1, -1):
+    while stack and nums[stack[-1]] >= nums[i]:
+        stack.pop()
+    indx[i] = stack[-1] if stack else -1
+    stack.append(i)
+print(*indx)
